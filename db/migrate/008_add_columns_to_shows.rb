4ccbe523be9e793e1d5955cfee7c57ca58bbf3a6
Suchitra Naik <susanaik@gmail.com>
2018-06-04 20:14:56 +0000

    automatically backed up by learn

---
+++ db/migrate/008_add_columns_to_shows.rb
@@ -0,0 +1,9 @@
+class AddColumnsToShows < ActiveRecord::Migration[4.2]
+  def change
+    add_column :shows, :genre, :string
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
+    
+    add_column :shows, :character_id, :integer
+  end
+end
